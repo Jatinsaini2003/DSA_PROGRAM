52b6167728621a87b5a38ca5e67c69dfa4764530
Jatin Saini <162905857+Jatinsaini2003@users.noreply.github.com>
2025-06-12 17:03:57 +0530

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n1, n2;
+    cout << "The numbers are : ";
+    cin >> n1 >> n2;
+    // first way
+
+    /*int gcd = 1;
+     for(int i = 1; i <= min(n1,n2); i++){
+         if(n1 % i == 0 && n2 % i == 0){
+             gcd = i;
+             }
+     }
+     cout << gcd; */
+
+    // second way
+   /* int gcd;
+    for(int i = min(n1,n2); i >= 1; i--){
+        if(n1 % i == 0 && n2 % i == 0){
+             gcd = i;
+            break;
+        }
+    }
+    cout << gcd; */
+    
+    // third way
+    while(n1 > 0 && n2 > 0){
+        if(n1 > n2){
+            n1 = n1 % n2;
+        }
+        else{
+           n2 = n2 % n1;
+        }
+        if(n1 == 0){
+            cout << n2;
+            
+        }
+        else {
+            cout << n1;
+           
+        }
+    }
+    return 0;
+}
