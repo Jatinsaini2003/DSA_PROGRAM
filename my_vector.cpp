adef75d0650968e561377657e12d195aa0b01d05
Jatin Saini <162905857+Jatinsaini2003@users.noreply.github.com>
2025-08-06 19:09:15 +0530

    Add files via upload

---
+++ my_vector.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Vector{
+    public:
+    int size;
+    int capacity;
+    int* arr;
+    Vector(){
+        size = 0;
+        capacity = 1;
+        arr = new int[1];
+    }
+    void add(int ele){
+        if(size == capacity){
+            capacity *= 2;
+            int* arr2 = new int[capacity];
+            for(int i = 0; i < size; i++){
+                arr2[i] = arr[i];
+            }
+            arr = arr2;
+        }
+        arr[size++] = ele;
+    }
+    void print(){
+        for(int i = 0; i < size; i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    int get(int idx){
+        if(size == 0){
+            cout << "Array is empty !" << endl;
+            return -1;
+        }
+        if(idx > size || idx < 0){
+            cout << "Array is invalid !" << endl;
+            return -1;
+        }
+        return arr[idx];
+    }
+    void remove(){
+        if(size == 0){
+            cout << "Array is empty !" << endl;
+        }
+        size--;
+    }
+
+};
+int main(){
+    Vector v;
+    cout << v.size << " " << v.capacity << endl;
+    v.add(10);
+    v.print();
+    cout << v.size << " " << v.capacity << endl;
+    v.add(11);
+    v.print();
+    cout << v.size << " " << v.capacity << endl;
+    v.add(12);
+    v.print();
+    cout << v.size << " " << v.capacity << endl;
+    v.add(13);
+    v.print();
+    cout << v.size << " " << v.capacity << endl;
+    v.add(14);
+    v.print();
+    cout << v.size << " " << v.capacity << endl;
+    v.add(15);
+    v.print();
+    cout << v.size << " " << v.capacity << endl;
+    v.get(10);
+    return 0;
+}
